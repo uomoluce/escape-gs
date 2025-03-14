5f2ab6fa4492ec23bf6d24ad484addd1b8ec9b4b
milena <milena.zuccarelli@gmail.com>
2025-03-14 22:42:56 +0200

    changes for prod

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250314_195529 from './20250314_195529';
+import * as migration_20250314_203911 from './20250314_203911';
 
 export const migrations = [
   {
     up: migration_20250314_195529.up,
     down: migration_20250314_195529.down,
-    name: '20250314_195529'
+    name: '20250314_195529',
+  },
+  {
+    up: migration_20250314_203911.up,
+    down: migration_20250314_203911.down,
+    name: '20250314_203911'
   },
 ];
