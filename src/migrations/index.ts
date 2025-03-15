a8bde30f2a5ca3f9c8f713d4e76a7bd589b29145
milena <milena.zuccarelli@gmail.com>
2025-03-15 19:40:03 +0200

    migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,7 +1,7 @@
 import * as migration_20250314_195529 from './20250314_195529';
 import * as migration_20250314_203911 from './20250314_203911';
 import * as migration_20250315_150926 from './20250315_150926';
-import * as migration_20250315_173131 from './20250315_173131';
+import * as migration_20250315_173939 from './20250315_173939';
 
 export const migrations = [
   {
@@ -20,8 +20,8 @@ export const migrations = [
     name: '20250315_150926',
   },
   {
-    up: migration_20250315_173131.up,
-    down: migration_20250315_173131.down,
-    name: '20250315_173131'
+    up: migration_20250315_173939.up,
+    down: migration_20250315_173939.down,
+    name: '20250315_173939'
   },
 ];
